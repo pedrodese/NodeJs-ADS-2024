49bbb3ad21feec8b002f3ce5e7b0b2f558ad1496
pedrodese <pedrohenriquedasilvapereira12@gmail.com>
2024-03-05 21:32:36 -0300

    Rota Finalizada

--- ListaDeGames/index.js
+++ ListaDeGames/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
 
 app.listen(3080,() => {
     console.log("Servidor Rodando!");
@@ -17,8 +19,21 @@ let games = [
     {title: "Microsoft Flight Simulator", studio: "Microsoft", price: 299},
     {title: "WYD", studio: "RaidHut", price: 0},
     {title: "Halo", studio: "Microsoft", price: 90}
-]
+];
 
 app.get("/",(req, res) => {
     res.json(games);
-})
+});
+
+app.post("/novogame",(req, res) => {
+    let title = req.body.title
+    let studio = req.body.studio
+    let price = req.body.price
+
+    let newGame = {title, studio, price};
+
+    games.push(newGame);
+
+    res.send("OK");
+});
+
