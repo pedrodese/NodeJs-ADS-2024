c0ebf39b33a7d50f85cf891c1fd4c72e1679a0b8
pedrodese <pedrohenriquedasilvapereira12@gmail.com>
2024-03-11 20:54:30 -0300

    ApiRest Finalizada

---
+++ apiRest/dados/dados.js
@@ -0,0 +1,39 @@
+const colecaoUf = [
+    {
+        id: 1,
+        uf: "AC",
+        nome: "Acre"
+    },
+    {
+        id: 2,
+        uf: "MT",
+        nome: "Mato Grosso" 
+    },
+    {
+        id: 3,
+        uf: "SC",
+        nome: "Santa Catarina"
+    },
+    {
+        id: 4,
+        uf: "RJ",
+        nome: "Rio de Janeiro"
+    },
+    {
+        id: 5,
+        uf: "RS",
+        nome: "Rio Grande do Sul"
+    },
+    {
+        id: 6,
+        uf: "RN",
+        nome: "Rio Grande do Norte"
+    },
+    {
+        id: 7,
+        uf: "SP",
+        nome: "São Paulo"
+    }
+]
+
+export default colecaoUf
